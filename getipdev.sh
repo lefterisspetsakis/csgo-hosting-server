6a29578662b235fee4051bc41b49e5325c8ab94b
csadmin <left.plan@gmail.com>
2017-02-27 15:16:33 -0500

    Fixed comparison error

--- getipdev.sh
+++ getipdev.sh
@@ -10,21 +10,28 @@ while [ true ]
 do
 	#check if the file exists
 	if test -e "$FILENAME";then
+		echo "File exists"
 		#get ip
 		IP="`wget -q -O - checkip.dyndns.org|sed -e 's/.*Current IP Address: //' -e 's/<.*$//'`"
+		contents=$(cat myip)
 		#if ip is different from stored ip, send email
-		if [ "$IP" != "$FILENAME" ]
+		if [ "$IP" != "$contents" ]; then
 			echo "sending email..."
 			date +"%m/%d/%Y %H:%M:%S $HOSTNAME"
 			echo "$IP"
 			echo "$IP" > "$FILENAME"
 			ssmtp left.plan@gmail.com < myip
 			ssmtp nimrodvanir97@gmail.com < myip
+		else
+			echo "IP has not changed"
 		fi
 	#if myip file doesnt exist, create one
 	#TODO: wrap email code in a function and call it every time there is a change in the ip
-	else;then
+	else
+		echo "File does not exist"
+		echo "Checking ip address..."
 		wget -q -O - checkip.dyndns.org|sed -e 's/.*Current IP Address: //' -e 's/<.*$//' > ~/myip
+		echo "Done"
 	fi
 	
 	#date +"%m/%d/%Y %H:%M:%S $HOSTNAME"
@@ -37,5 +44,5 @@ do
 	#TODO: test sleep time with 30 seconds (so that this script
 	#	checks for an ip change every 30 seconds and notifies
 	#	as necessary)
-	sleep 1
+	sleep 2
 done
